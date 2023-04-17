c8f021f1e538966e8859e21b61d5e9ecc19b85f3
fivip.saefulloh <fvpsaefulloh@gmail.com>
2023-04-17 13:24:02 +0700

    adding some test

--- src/test/java/com/accenture/assessment/bni/service/UserServiceTests.java
+++ src/test/java/com/accenture/assessment/bni/service/UserServiceTests.java
@@ -1,6 +1,7 @@
 package com.accenture.assessment.bni.service;
 
 import com.accenture.assessment.bni.controller.request.UserRequest;
+import com.accenture.assessment.bni.controller.response.UserPagedResponse;
 import com.accenture.assessment.bni.controller.response.UserResponse;
 import com.accenture.assessment.bni.dto.UserDto;
 import com.accenture.assessment.bni.entity.QUser;
@@ -13,7 +14,6 @@ import com.accenture.assessment.bni.repository.UserRepository;
 import com.accenture.assessment.bni.repository.custom.UserCustomRepository;
 import com.accenture.assessment.bni.service.impl.UserServiceImpl;
 import com.querydsl.core.BooleanBuilder;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -25,11 +25,9 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.time.LocalDate;
 import java.time.ZonedDateTime;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.when;
 
@@ -70,16 +68,16 @@ public class UserServiceTests {
     }
 
     @Test
-    public void whenSaveUSerWithValidDataThenDataSavedSuccessfully() {
+    public void whenSaveUserWithValidDataThenDataSavedSuccessfully() {
         when(repository.save(any())).thenReturn(user);
         when(userSettingService.save(any())).thenReturn(getUserSettingList());
 
         UserResponse userResponse = userService.save(userRequest);
         UserDto userDto = userResponse.getUserData();
-        Assertions.assertEquals(userRequest.getBirthDate(), userDto.getBirthDate());
-        Assertions.assertEquals(userRequest.getFirstName(), userDto.getFirstName());
-        Assertions.assertEquals(userRequest.getLastName(), userDto.getLastName());
-        Assertions.assertEquals(16, userDto.getSsn().length());
+        assertEquals(userRequest.getBirthDate(), userDto.getBirthDate());
+        assertEquals(userRequest.getFirstName(), userDto.getFirstName());
+        assertEquals(userRequest.getLastName(), userDto.getLastName());
+        assertEquals(16, userDto.getSsn().length());
     }
 
     @Test
@@ -90,10 +88,10 @@ public class UserServiceTests {
 
         UserResponse userResponse = userService.getUser(user.getId());
         UserDto userDto = userResponse.getUserData();
-        Assertions.assertEquals(userRequest.getBirthDate(), userDto.getBirthDate());
-        Assertions.assertEquals(userRequest.getFirstName(), userDto.getFirstName());
-        Assertions.assertEquals(userRequest.getLastName(), userDto.getLastName());
-        Assertions.assertEquals(16, userDto.getSsn().length());
+        assertEquals(userRequest.getBirthDate(), userDto.getBirthDate());
+        assertEquals(userRequest.getFirstName(), userDto.getFirstName());
+        assertEquals(userRequest.getLastName(), userDto.getLastName());
+        assertEquals(16, userDto.getSsn().length());
     }
 
     @Test
@@ -106,10 +104,10 @@ public class UserServiceTests {
 
         UserResponse userResponse = userService.update(userRequest, user.getId());
         UserDto userDto = userResponse.getUserData();
-        Assertions.assertEquals(userRequest.getBirthDate(), userDto.getBirthDate());
-        Assertions.assertEquals(userRequest.getFirstName(), userDto.getFirstName());
-        Assertions.assertEquals(userRequest.getLastName(), userDto.getLastName());
-        Assertions.assertEquals(16, userDto.getSsn().length());
+        assertEquals(userRequest.getBirthDate(), userDto.getBirthDate());
+        assertEquals(userRequest.getFirstName(), userDto.getFirstName());
+        assertEquals(userRequest.getLastName(), userDto.getLastName());
+        assertEquals(16, userDto.getSsn().length());
     }
 
     @Test
@@ -131,29 +129,78 @@ public class UserServiceTests {
 
         UserResponse userResponse = userService.activateUser(inActiveUser.getId());
         UserDto userDto = userResponse.getUserData();
-        Assertions.assertEquals(true, userDto.getIsActive());
+        assertEquals(true, userDto.getIsActive());
+
     }
 
     @Test
     public void whenGetPagedUserThenUserPagedDataReturnedBasedOnMaxRecordsAndOffset() {
-        User inActiveUser = new User();
-        inActiveUser.setIsActive(false);
-        inActiveUser.setDeletedTime(ZonedDateTime.now());
-        inActiveUser.setFirstName("Firstname");
-        inActiveUser.setFamilyName("FamilyName");
-        inActiveUser.setSsn("0000001234554321");
-        inActiveUser.setUpdatedTime(ZonedDateTime.now().minusHours(4));
-        inActiveUser.setCreatedTime(ZonedDateTime.now().minusDays(1));
-        inActiveUser.setCreatedBy("SYSTEM");
-        inActiveUser.setId(3L);
 
+        User dummyUser = new User();
+        dummyUser.setIsActive(false);
+        dummyUser.setDeletedTime(ZonedDateTime.now());
+        dummyUser.setFirstName("Firstname");
+        dummyUser.setFamilyName("FamilyName");
+        dummyUser.setSsn("0000001234554321");
+        dummyUser.setUpdatedTime(ZonedDateTime.now().minusHours(4));
+        dummyUser.setCreatedTime(ZonedDateTime.now().minusDays(1));
+        dummyUser.setCreatedBy("SYSTEM");
+        dummyUser.setId(3L);
+
+        List<User> users = new ArrayList<>();
+
+        users.add(dummyUser);
+        users.add(user);
+
+        Integer maxRecords = 10;
+        Integer offset = 0;
+
+        when(userCustomRepository.getActiveUser(maxRecords,offset)).thenReturn(users);
+
+        UserPagedResponse userResponse = userService.getPagedActiveUser(maxRecords,offset);
+        assertEquals(maxRecords, userResponse.getMaxRecords());
+        assertEquals(offset, userResponse.getOffset());
+
+        assertEquals(2, userResponse.getUserData().size());
+    }
+
+    @Test
+    public void whenDeleteActiveUserThenUserSuccessfullyDeleted() {
+
+        User dummyUser = new User();
+        dummyUser.setIsActive(true);
+        dummyUser.setDeletedTime(ZonedDateTime.now());
+        dummyUser.setFirstName("Firstname");
+        dummyUser.setFamilyName("FamilyName");
+        dummyUser.setSsn("0000001234554321");
+        dummyUser.setUpdatedTime(ZonedDateTime.now().minusHours(4));
+        dummyUser.setCreatedTime(ZonedDateTime.now().minusDays(1));
+        dummyUser.setCreatedBy("SYSTEM");
+        dummyUser.setId(1L);
+
+        Long id = 1L;
         BooleanBuilder builder = new BooleanBuilder();
-        builder.and(qUser.isActive.isFalse()).and(qUser.id.eq(inActiveUser.getId()));
-        when(repository.findOne(builder)).thenReturn(Optional.of(inActiveUser));
+        builder.and(qUser.isActive.isTrue()).and(qUser.id.eq(id));
+        when(repository.findOne(builder)).thenReturn(Optional.of(dummyUser));
 
-        UserResponse userResponse = userService.activateUser(inActiveUser.getId());
-        UserDto userDto = userResponse.getUserData();
-        Assertions.assertEquals(true, userDto.getIsActive());
+        userService.delete(id);
+
+        assertEquals(false, dummyUser.getIsActive());
+        assertEquals(true, dummyUser.getDeletedTime() != null);
+    }
+
+    @Test
+    public void whenUpdateUserSettingsThenUserSettingsUpdatedSuccessfully() {
+        Long id = 1L;
+        BooleanBuilder builder = new BooleanBuilder();
+        builder.and(qUser.isActive.isTrue()).and(qUser.id.eq(id));
+        when(repository.findOne(builder)).thenReturn(Optional.of(user));
+        ZonedDateTime current = ZonedDateTime.now();
+
+
+        UserResponse userResponse = userService.updateSettings(getUserSettingsForUpdate(), id);
+
+        assertEquals(true, current.isBefore(userResponse.getUserData().getUpdatedTime()));
     }
 
     private List<UserSetting> getUserSettingList() {
@@ -166,5 +213,17 @@ public class UserServiceTests {
                 }
         );
         return userSettingList;
+    }
+
+    private List<Map<String, Object>> getUserSettingsForUpdate() {
+        List<Map<String, Object>> userSettingsList = new ArrayList<>();
+        Arrays.asList(UserSettingsKeyEnum.values()).forEach(
+            (val) -> {
+                Map<String, Object> map = new HashMap<>();
+                map.put(val.getName(), val.getDefaultValue());
+                userSettingsList.add(map);
+            }
+        );
+        return userSettingsList;
     }
 }
